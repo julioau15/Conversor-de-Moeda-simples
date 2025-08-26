985437624d59de7e4cf3d5b64cf1e8130b327367
julioau15 <julioaugustov@gmail.com>
2025-08-26 08:42:20 -0300

    Primeiro commit, adicionado código simples java pela ide Intellij

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class ConversorDeMoeda {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Executando o aplicativo...");
+
+        CalculoCambio  calculo = new CalculoCambio();
+
+        CalculoCambio.obterDados();
+    }
+}
